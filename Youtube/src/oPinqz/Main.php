4c82e0fde4ec639660f1be4a410ae1152b6e22eb
oPinqz <82214170+iAbdo2008@users.noreply.github.com>
2024-06-06 10:54:44 +0300

    New Video

--- Youtube/src/oPinqz/Main.php
+++ Youtube/src/oPinqz/Main.php
@@ -5,16 +5,34 @@ namespace oPinqz;
 use pocketmine\Server;
 use pocketmine\player\Player;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 
-class Main extends PluginBase {
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
 
     public function onEnable() : void {
         $this->getServer()->getLogger()->info("§aAna Ashta3'lt");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onDisable() : void {
         $this->getServer()->getLogger()->info("§aAna Atafet");
+    }
+
+    public function onJoin(PlayerJoinEvent $e) {
+        $player = $e->getPlayer();
+
+        $player->sendMessage("Anta D5lt");
+        $e->setJoinMessage($player->getName() . " D5l al Server");
+    }
+
+    public function onQuit(PlayerQuitEvent $e) {
+        $player = $e->getPlayer();
+
+        $e->setQuitMessage($player->getName() . " Tl3 mn al Server");
     }
 
 }
